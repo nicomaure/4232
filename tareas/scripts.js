02d22cf1cf5c7feb835e9d2727a4f60f722e5a8f
nicomaure <nicomau@gmail.com>
2025-06-25 23:32:33 -0300

    nueva edicion

--- tareas/scripts.js
+++ tareas/scripts.js
@@ -72,7 +72,7 @@ document.addEventListener('DOMContentLoaded', () => {
                                 <a href="${subject.driveLink}" target="_blank" class="action-button go-drive">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
                                 <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
-                                <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
+                                <path d="M5 5a2 2 0 00-2 2v11a2 2 0 002 2h8a2 2 0 002-2V5a2 2 0 00-2-2H6zM8 7a1 1 0 011-1h2a1 1 0 110 2H9a1 1 0 01-1-1z" />
                                 </svg>
                                 <span>Ver</span>
                                 </a>
