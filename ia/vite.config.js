1dd2fc12c33f107bbff8ce9f53360bf9e439989e
nicomaure <nicomau@gmail.com>
2026-02-26 22:23:25 -0300

    mover subcarpeta ia

--- ia/vite.config.js
+++ ia/vite.config.js
@@ -6,5 +6,5 @@ export default defineConfig({
   plugins: [react()],
   // Configura la URL base para el despliegue en GitHub Pages.
   // Reemplaza '4232' con el nombre exacto de tu repositorio de GitHub.
-  base: '/4232/',
+  base: '/4232/ia/',
 });
